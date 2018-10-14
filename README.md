3fdba6b91a583265b15f719279f232bd8952d8db
Renato Lessa da Silva <renatolessa.2011@hotmail.com>
2018-10-14 17:21:14 -0300

    Api Rest com Java8, SrpingBoot, SpringSecurity e Mongo

---
+++ README.md
@@ -0,0 +1,8 @@
+# App-Rels
+
+Api Restful 
+
+- Java 8 
+- Spring Boot 2.0.5
+- Spring Security
+- MongoDB
---
+++ src/test/java/com/relesi/rels/api/RelsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.relesi.rels.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RelsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
